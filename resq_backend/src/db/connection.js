3c1ac9b61c5f180cc510901abe3c190cffd32c1b
Clint700 <clintonchiemeka@gmail.com>
2025-03-30 06:11:53 +0100

    Backend Database successfully move to supabase

--- resq_backend/src/db/connection.js
+++ resq_backend/src/db/connection.js
@@ -1,18 +1,26 @@
 const { Pool } = require("pg");
-const ENV = process.env.NODE_ENV || "development";
+const path = require("path");
 
+// Ensure environment variables are loaded
 require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
+  path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV || "development"}`),
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  console.error("PGDATABASE or DATABASE_URL not set!");
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 const config = {};
 
-if (ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.ssl = { rejectUnauthorized: false };
+} else {
+  config.database = process.env.PGDATABASE;
 }
-module.exports = new Pool(config);
+
+const pool = new Pool(config);
+
+module.exports = pool;
